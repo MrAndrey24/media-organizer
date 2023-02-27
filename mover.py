1e74172d9fdcb98b54e3abdb01b20f941f46f225
Andrey Acosta  R <110435627+MrAndrey24@users.noreply.github.com>
2023-02-27 14:26:06 -0600

    Add files via upload

---
+++ mover.py
@@ -0,0 +1,37 @@
+# PIL: python3 -m pip install Pillow on MAC, Windows python -m pip install Pillow
+from PIL import Image
+from pathlib import Path
+
+DOWNLOADS_FOLDER = Path("C:/Users/Andrey Sam/Downloads")
+PICTURES_FOLDER = Path("C:/Users/Andrey Sam/Pictures")
+MUSIC_FOLDER = Path("C:/Users/Andrey Sam/Music")
+
+
+def compress_images():
+    """Compresses image files in the downloads folder and moves them to the pictures folder."""
+    for file_path in DOWNLOADS_FOLDER.glob("*"):
+        name, extension = file_path.stem, file_path.suffix.lower()
+
+        if extension in (".jpg", ".jpeg", ".png"):
+            try:
+                with Image.open(file_path) as img:
+                    img.save(PICTURES_FOLDER / f"compressed_{file_path.name}",
+                             optimize=True, quality=60)
+            except:
+                print(f"Failed to open {file_path}")
+            else:
+                file_path.unlink()  # remove original file
+                print(
+                    f"{name}{extension} was compressed and moved to {PICTURES_FOLDER}")
+
+
+def move_music():
+    """Moves music files in the downloads folder to the music folder."""
+    for file_path in DOWNLOADS_FOLDER.glob("*.mp3"):
+        file_path.rename(MUSIC_FOLDER / file_path.name)
+        print(f"{file_path.name} was moved to {MUSIC_FOLDER}")
+
+
+if __name__ == "__main__":
+    compress_images()
+    move_music()
